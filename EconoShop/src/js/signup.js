1fad0011119762828d55537b26afdc3841fd4758
DWAN-00 <kkw1700@gmail.com>
2023-06-10 01:16:39 +0900

    feat: signup, login 공백검사, 정규식 검사

--- EconoShop/src/js/signup.js
+++ EconoShop/src/js/signup.js
@@ -1,35 +1,84 @@
-var express = require('express');
-var app = express();
-
-// set the view engine to ejs
-app.set('view engine', 'ejs');
-app.use(express.static("views"));
-app.use(express.static('static'));
-
-// use res.render to load up an ejs view file
-app.use(express.json())
-// index page
-/* router.get('/register',function(req,res,next) {
-  var isOwner = authIsOwner(req,res);
-  if(!isOwner){
-    res.render('/login');
-  }else{
-    res.redirect('/');
+const inputId = document.querySelector(".input-id");
+const inputPw = document.querySelector(".input-pw");
+const inputPwr = document.querySelector(".input-pwrepeat");
+const inputEmail = document.querySelector(".input-email");
+const inputSemester = document.querySelector(".input-semester");
+const inputForm = document.querySelector(".input-form");
+//공백검사
+function inputIdAlert(event) {
+  const idInvalidCheck = document.querySelector(".id-invalid-check");
+  if (inputId.value == "") {
+    event.preventDefault();
+    inputId.classList.add("input-alert");
+    idInvalidCheck.classList.add("invalid-visible");
+  } else if (inputId.value !== "") {
+    inputId.classList.remove("input-alert");
+    idInvalidCheck.classList.remove("invalid-visible");
   }
-});  */
+}
 
-app.post('/register', function(req,res) {
-  console.log(req.body);
-  const { content } = req.body
-  comments.push(content)
-  console.log(comments);
-  res.redirect('/')
-});
+function inputPwAlert(event) {
+  const pwInvalidCheck = document.querySelector(".pw-invalid-check");
+  if (inputPw.value == "") {
+    event.preventDefault();
+    inputPw.classList.add("input-alert");
+    pwInvalidCheck.classList.add("invalid-visible");
+  } else if (inputPw.value !== "") {
+    inputPw.classList.remove("input-alert");
+    pwInvalidCheck.classList.remove("invalid-visible");
+  }
+}
 
-// about page
-app.get('/about', function(req, res) {
-  res.render('pages/about');
-});
+function inputPwrAlert(event) {
+  const pwrInvalidCheck = document.querySelector(".pwr-invalid-check");
+  if (inputPwr.value == "") {
+    event.preventDefault();
+    inputPwr.classList.add("input-alert");
+    pwrInvalidCheck.classList.add("invalid-visible");
+  } else if (inputPwr.value !== "") {
+    inputPwr.classList.remove("input-alert");
+    pwrInvalidCheck.classList.remove("invalid-visible");
+  }
+}
 
-app.listen(3000);
-console.log('Server is listening on port 3000');
+function inputEmailAlert(event) {
+  const emailInvalidCheck = document.querySelector(".email-invalid-check");
+  const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  if (inputEmail.value == "") {
+    event.preventDefault();
+    inputEmail.classList.add("input-alert");
+    emailInvalidCheck.classList.add("invalid-visible");
+  } else if (!emailRegex.test(inputEmail.value)) {
+    event.preventDefault();
+    inputEmail.classList.add("input-alert");
+    emailInvalidCheck.classList.add("invalid-visible");
+    emailInvalidCheck.innerHTML = "PLEASE CHECK YOUR EMAIL";
+  } else if (inputEmail.value !== "") {
+    inputEmail.classList.remove("input-alert");
+    emailInvalidCheck.classList.remove("invalid-visible");
+  }
+}
+
+function inputSemesterAlert(event) {
+  const semesterInvalidCheck = document.querySelector(
+    ".semester-invalid-check"
+  );
+  if (inputSemester.value == "") {
+    event.preventDefault();
+    inputSemester.classList.add("input-alert");
+    semesterInvalidCheck.classList.add("invalid-visible");
+  } else if (inputSemester.value !== "") {
+    inputSemester.classList.remove("input-alert");
+    semesterInvalidCheck.classList.remove("invalid-visible");
+  }
+}
+
+inputForm.addEventListener("submit", inputIdAlert);
+inputForm.addEventListener("submit", inputPwAlert);
+inputForm.addEventListener("submit", inputPwrAlert);
+inputForm.addEventListener("submit", inputEmailAlert);
+inputForm.addEventListener("submit", inputSemesterAlert);
+
+function goNewPage(newHref = "") {
+  window.location.href = newHref;
+}
