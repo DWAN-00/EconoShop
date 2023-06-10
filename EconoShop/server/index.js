087ece9fad53bbad92a1967ec4c27139b0c7baf6
DWAN-00 <kkw1700@gmail.com>
2023-06-10 21:53:08 +0900

    feat: mongoDB 연동 + 정규식 함수 추출 + #1 이슈 추가

--- EconoShop/server/index.js
+++ EconoShop/server/index.js
@@ -2,12 +2,12 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const bodyParser = require("body-parser");
-
+const mongoose = require("mongoose");
+const port = 3000;
 app.set("view engine", "ejs"); // 템플릿 엔진 설정
 app.set("views", path.join(__dirname, "views")); // 템플릿 파일이 위치한 폴더 경로 설정
 app.use(express.static(path.join(__dirname, "../src"))); // 정적 파일 제공 설정
-
-// use res.render to load up an ejs view file
+app.use(express.json({ extended: false }));
 
 app.use(express.json());
 app.get("/", (req, res) => {
@@ -23,15 +23,26 @@ app.use(express.json());
 app.get("/login", (req, res) => {
   res.render("login");
 });
-/*
-app.get("/", function (req, res, next) {
-  var isOwner = authIsOwner(req, res);
-  if (!isOwner) {
-    res.render("/login");
-  } else {
-    res.redirect("/");
-  }
+
+app.use(express.json());
+app.get("/mypage", (req, res) => {
+  res.render("mypage");
 });
-*/
-app.listen(3000);
-console.log("Server is listening on port 3000");
+
+const uri =
+  "mongodb+srv://admin:ZdKhAASw5N6U.YK@econoshop.tyhsydq.mongodb.net/?retryWrites=true&w=majority";
+
+async function connect() {
+  try {
+    await mongoose.connect(uri);
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+connect();
+
+app.listen(3000, () => {
+  console.log("Server started on 3000");
+});
---
+++ EconoShop/src/js/models/user.js
@@ -0,0 +1,5 @@
+// package
+const mongoose = require("mongoose");
+const config = require("config");
+// URI
+const uri = config.get("mongoURI");
